6c375a79d366de1b8dae3f57f93710be5e916eb7
Walker-TJA <jgaitken10@gmail.com>
2025-09-25 13:13:10 -0600

    Update installer.js

--- js/installer/installer.js
+++ js/installer/installer.js
@@ -1,101 +1,34 @@
-var Dialog = require('installer/dialog');
-var zip = require('lib/zip.js').zip;
-var Lh4 = require('lib/lh4.js');
-var assets = require('assets');
-
-var Installer = function() {
-    this.dialog = new Dialog('dialog');
-    this.isLocal = window.location.hostname.indexOf('localhost') !== -1;
-};
-
-// This localUrl modification is correct for skipping the download when running locally.
 Installer.localUrl = 'data/pak0.pak'; 
+// Remove or comment out mirrors and proxy, they are no longer needed
+// Installer.crossOriginProxyUrl = 'https://cors-anywhere.herokuapp.com/';
+// Installer.mirrors = []; 
 
-// This is no longer needed and can be removed or left empty.
-Installer.crossOriginProxyUrl = ''; 
-
-// Replace the old mirrors with a direct-download link.
-Installer.mirrors = [
-    'https://www.quaddicted.com/files/idgames/idstuff/quake/quake106.zip'
-];
-
-Installer.prototype.error = function(message) {
-    this.dialog.error(message);
-};
+// ...
 
 Installer.prototype.download = function(done) {
-    this.dialog.setCaption('Downloading shareware version of Quake (quake106.zip)...');
-    var url = this.isLocal ?
-        Installer.localUrl :
-        Installer.mirrors[0]; // Directly use the mirror URL
-    var unpacked = url.indexOf('pak') !== -1;
-
+    this.dialog.setCaption('Loading pak0.pak from local files...');
+    // Always use the local path
+    var url = Installer.localUrl;
+    var unpacked = true; // Set to true to skip the unpack function
+    
+    // Use XMLHttpRequest to load the local file
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
-    xhr.overrideMimeType('text/plain; charset=x-user-defined');
     xhr.responseType = 'arraybuffer';
 
     var self = this;
-    xhr.onreadystatechange = function() {
-        self.dialog.setCaption('Download completed. Processing file...', this.readyState);
-        xhr.onreadystatechange = null;
-    };
-
     xhr.onload = function(e) {
         if (this.status === 200) {
-
-            if (!unpacked) {
-                self.unpack(this.response, done);
-            } else {
-                self.finalize(new Uint8Array(this.response), done);
-            }
+            // Since we are loading the raw .pak file, we can finalize directly
+            self.finalize(new Uint8Array(this.response), done);
         } else {
-            self.error('Download failed. Try again.');
+            self.error('Failed to load local pak0.pak. Make sure the file exists in the /data folder.');
         }
     };
     xhr.send();
 };
 
+// Remove the unpack function, as it's no longer necessary
 Installer.prototype.unpack = function(response, done) {
-    var blob = new Blob([response]);
-    var self = this;
-    zip.workerScriptsPath = 'js/lib/';
-    zip.createReader(new zip.BlobReader(blob), function(reader) {
-        reader.getEntries(function(entries) {
-            if (entries.length <= 0 || entries[0].filename !== 'resource.1') {
-                self.dialog.error('Downloaded archive was corrupt.');
-                return;
-            }
-            var entry = entries[0];
-            var writer = new zip.ArrayWriter(entry.uncompressedSize);
-            self.dialog.setCaption('Extracting zip resources...');
-            entry.getData(writer, function(buffer) {
-                self.dialog.setCaption('Extracting lha resources...');
-                var lha = new Lh4.LhaReader(new Lh4.LhaArrayReader(buffer));
-                // Use lowercase and forward slashes for better compatibility.
-                var data = lha.extract('id1/pak0.pak'); 
-                self.finalize(data, done);
-            });
-        });
-    });
+    // This code path is now effectively unused
 };
-
-Installer.prototype.finalize = function(data, done) {
-    assets.setPak(data);
-    this.dialog.setCaption('Starting up Quake...');
-    this.dialog.hide();
-    done();
-};
-
-Installer.prototype.start = function(done) {
-    this.dialog.setCaption("Initiating download...");
-    this.dialog.show();
-    this.download(done);
-};
-
-module.exports = exports = new Installer();
-
-
-
-
-
